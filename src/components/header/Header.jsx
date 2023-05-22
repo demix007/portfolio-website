c54a14d8f7ba6349df2462c641495b5b89a84c02
demix007 <ayodejiabidemi007@gmail.com>
2023-05-22 19:29:46 +0100

    Add call to action buttons to the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+   <header>
+    <div className="container header_container">
+      <h5>Hello, I'm</h5>
+      <h1>Ayodeji Abidemi</h1>
+      <h5 className="text-light">Fullstack Web Developer</h5>
+      <CTA />
+    </div>
+   </header>
   )
 }
 
