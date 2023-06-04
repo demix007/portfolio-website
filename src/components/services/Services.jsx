290fd2575a0b48ff20c2b138a9f731774e4f010d
demix007 <ayodejiabidemi007@gmail.com>
2023-06-04 02:52:24 +0100

    Add Full-Stack Web Development service

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -57,6 +57,31 @@ const Services = () => (
           </li>
         </ul>
       </article>
+
+      <article className="service">
+        <div className="service__head">
+          <h3>Full-Stack Web Development</h3>
+        </div>
+
+        <ul className="service__list">
+          <li>
+            <BsCheck2Circle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BsCheck2Circle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BsCheck2Circle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+          <li>
+            <BsCheck2Circle className="service__list-icon" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </li>
+        </ul>
+      </article>
     </div>
   </section>
 );
