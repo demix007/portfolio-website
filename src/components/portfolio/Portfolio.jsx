555b6654046e3a5c8eaa5ba307d73d52d8411b96
demix007 <ayodejiabidemi007@gmail.com>
2023-06-05 16:45:15 +0100

    Add portfolio section Markup and map through tthe array of projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -65,5 +65,49 @@ const Portfolio = () => {
       demo: 'https://cars-rental-front-end-rose.vercel.app/',
     },
   ];
+
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({
+            id,
+            image,
+            title,
+            description,
+            technologies,
+            source,
+            demo,
+          }) => (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-description">
+                <p>{description}</p>
+              </div>
+              <div className="portfolio__item-technologies">
+                {technologies.map((tech) => (
+                  <small key={uuidv4()}>{tech}</small>
+                ))}
+              </div>
+              <div className="portfolio__item-cta">
+                <a href={source} className="btn" target="_blank" rel="noreferrer">
+                  Source Code
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank" rel="noreferrer">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          ))
+        }
+      </div>
+    </section>
+  );
 };
 export default Portfolio;
