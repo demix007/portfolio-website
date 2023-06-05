2f6043e5c5790185dea1e8d09366a209c76872bc
demix007 <ayodejiabidemi007@gmail.com>
2023-06-05 16:42:39 +0100

    import live projects screenshots into portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import './portfolio.css'
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import './portfolio.css';
+import SpaceTravelers from '../../assets/spacetravelers.png';
+import Bookstore from '../../assets/bookstore.png';
+import AwesomeBooks from '../../assets/awesome_books.png';
+import CountryFinder from '../../assets/country_finder.png';
+import Leaderboard from '../../assets/leaderboard.png';
+import CarRentals from '../../assets/car_rentals.png';
 
 const Portfolio = () => {
-  return (
-    <section id='portfolio'>Portfolio</section>
-  )
-}
-
-export default Portfolio
+  };
+export default Portfolio;
