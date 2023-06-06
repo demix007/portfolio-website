38b2d1cae274c5504c6f5370faf71cc91dde8434
demix007 <ayodejiabidemi007@gmail.com>
2023-06-06 05:39:00 +0100

    Add contact options markup

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import './contact.css';
+import { MdOutlineMarkEmailRead } from 'react-icons/md';
+import { BsWhatsapp } from 'react-icons/bs';
 
-const Contact = () => (
-  <section id="contact">Contact</section>
-);
+const Contact = () => {
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMarkEmailRead className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>ayodejiabidemi007@gmail.com</h5>
+            <a href="mailto:ayodejiabidemi007@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+2348163046727</h5>
+            <a href="https://api.whatsapp.com/send?phone=2348163046727" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Contact;
