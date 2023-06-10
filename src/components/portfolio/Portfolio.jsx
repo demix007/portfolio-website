3ba24fe7c69235caca92475e618266eb1ebbda69
demix007 <ayodejiabidemi007@gmail.com>
2023-06-10 01:26:37 +0100

    Add react-crypto-api project to portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,6 +7,7 @@ import AwesomeBooks from '../../assets/awesome_books.png';
 import CountryFinder from '../../assets/country_finder.png';
 import Leaderboard from '../../assets/leaderboard.png';
 import CarRentals from '../../assets/car_rentals.png';
+import CRYPTO from '../../assets/crypto.png';
 
 const Portfolio = () => {
   const data = [
@@ -24,12 +25,21 @@ const Portfolio = () => {
       image: CarRentals,
       title: 'Car Rentals',
       description: 'This project is based on an app to book an appointment to try a dream car. App users are able to reserve their dream cars, as well as view the reservations. New cars can also be added and deleted from the app. Postgres database was used as database, Rails to create backend API, and React & Redux for frontend UI',
-      technologies: ['React', 'Ruby On Rails', 'PostgreSQL', 'Bootstrap', 'CSS'],
+      technologies: ['React', 'Rails', 'PostgreSQL', 'Bootstrap', 'CSS'],
       source: 'https://github.com/demix007/car-rentals-backend',
       demo: 'https://cars-rental-front-end.vercel.app/',
     },
     {
       id: 3,
+      image: CRYPTO,
+      title: 'React Crypto API',
+      description: 'This is a react API project that displays the top 6 cryptocurrencies by market capitalization. The data is fetched from the CoinGecko API. The project is built with React, HTML, and CSS.',
+      technologies: ['React', 'HTML', 'CSS'],
+      source: 'https://github.com/demix007/react-crypto-api',
+      demo: 'https://react-crypto-api-sand.vercel.app/',
+    },
+    {
+      id: 4,
       image: AwesomeBooks,
       title: 'Awesome Books',
       description: 'Awesome Books is a professional JavaScript application that enables users to efficiently add and remove books from a list, providing a seamless experience for book management.',
@@ -38,7 +48,7 @@ const Portfolio = () => {
       demo: 'https://awesome-books-es-6-chi.vercel.app/',
     },
     {
-      id: 4,
+      id: 5,
       image: Bookstore,
       title: 'Bookstore',
       description: 'Book Store is a React app for seamless book and author management, seamlessly integrating with an API. Users can efficiently add and remove books with ease, ensuring a smooth experience for book enthusiasts.',
@@ -47,7 +57,7 @@ const Portfolio = () => {
       demo: 'https://bookstore-cyan.vercel.app/',
     },
     {
-      id: 5,
+      id: 6,
       image: Leaderboard,
       title: 'Leaderboard App',
       description: 'Here, I set up a JavaScript project for the Leaderboard list app, using webpack and ES6. I developed the app following a wireframe, and consumed the Leaderboard API using JavaScript async and await.',
@@ -56,7 +66,7 @@ const Portfolio = () => {
       demo: 'https://enchanting-pavlova-7392f8.netlify.app/',
     },
     {
-      id: 6,
+      id: 7,
       image: CountryFinder,
       title: 'Global Country Finder',
       description: 'This project is a react web application which has accessible data for different countries around the world. These data include region, name, population, sub-region, capital and more.Countries can be searched or filtered by region.',
